cd9ccaf45837d8ecfe194f788b5649f64cb56e76
ACE <42440432+ACEx86@users.noreply.github.com>
2025-02-09 23:00:10 +0200

    Fix ignoring system dns anyway if bootstrap resolvers is empty

--- dnscrypt-proxy/config.go
+++ dnscrypt-proxy/config.go
@@ -398,8 +398,8 @@ func ConfigLoad(proxy *Proxy, flags *ConfigFlags) error {
 				return fmt.Errorf("Bootstrap resolver [%v]: %v", resolver, err)
 			}
 		}
-		proxy.xTransport.ignoreSystemDNS = config.IgnoreSystemDNS
 	}
+	proxy.xTransport.ignoreSystemDNS = config.IgnoreSystemDNS
 	proxy.xTransport.bootstrapResolvers = config.BootstrapResolvers
 	proxy.xTransport.useIPv4 = config.SourceIPv4
 	proxy.xTransport.useIPv6 = config.SourceIPv6
